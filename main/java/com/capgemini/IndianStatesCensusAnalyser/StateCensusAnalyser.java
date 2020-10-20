460b1b78b62629d61a4e31b1348917e91561631e
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-20 20:31:46 +0530

    added: Created classes to load the Indian State Census Information

--- main/java/com/capgemini/IndianStatesCensusAnalyser/StateCensusAnalyser.java
+++ main/java/com/capgemini/IndianStatesCensusAnalyser/StateCensusAnalyser.java
@@ -1,7 +1,31 @@
 package com.capgemini.IndianStatesCensusAnalyser;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
 public class StateCensusAnalyser {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Indian State Census Analyser Problem");
+	public int loadIndiaCensusData(String csvFilePath) throws Exception {
+		try {
+			Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+			CsvToBeanBuilder<IndianCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+			csvToBeanBuilder.withType(IndianCensusCSV.class);
+			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			CsvToBean<IndianCensusCSV> csvToBean = csvToBeanBuilder.build();
+			Iterator<IndianCensusCSV> censusCSVIterator = csvToBean.iterator();
+			int numOfEntries = 0;
+			while (censusCSVIterator.hasNext()) {
+				numOfEntries++;
+			}
+			return numOfEntries;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return 0;
+		}
 	}
 }
